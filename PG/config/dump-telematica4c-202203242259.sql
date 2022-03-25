fa1dfce4e44950fc2c139467f1fdc3ae40cbb234
AMRODOLFODANIEL <79113659+AMRODOLFODANIEL@users.noreply.github.com>
2022-03-24 23:03:20 -0600

    Examen P1
    EXAMEN P1

--- PG/config/dump-telematica4c-202203242259.sql
+++ PG/config/dump-telematica4c-202203242259.sql
@@ -16,6 +16,32 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `pelis`
+--
+
+DROP TABLE IF EXISTS `pelis`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pelis` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `titulo` varchar(250) NOT NULL,
+  `descripcion` varchar(250) NOT NULL,
+  `fecha` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pelis`
+--
+
+LOCK TABLES `pelis` WRITE;
+/*!40000 ALTER TABLE `pelis` DISABLE KEYS */;
+INSERT INTO `pelis` VALUES (15,'xd','hola','2022-06-25'),(16,'Batman ','Pelicula de batman ','2022-03-03');
+/*!40000 ALTER TABLE `pelis` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -28,7 +54,7 @@ CREATE TABLE `users` (
   `email` varchar(45) DEFAULT NULL,
   `pass` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +63,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'root|','ralcala@ucol.mx','454b9be3be076694743eaae2b29430f6');
+INSERT INTO `users` VALUES (1,'root|','ralcala@ucol.mx','454b9be3be076694743eaae2b29430f6'),(2,'Daniel','alcaladaniel4@gmail.com','25f9e794323b453885f5181f1b624d0b'),(3,'FairerGrunt26','dani-181202@hotmail.com','25d55ad283aa400af464c76d713c07ad'),(4,'danieeel','dhfhfhddfh@gmail.com','25f9e794323b453885f5181f1b624d0b');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-03-20 10:36:51
+-- Dump completed on 2022-03-24 22:59:24
